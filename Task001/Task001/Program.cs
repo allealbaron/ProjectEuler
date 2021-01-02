d7f173985422b17d014a9133a9c4cc8ee925911b
Angel <angel.luis.leal.baron@gmail.com>
2021-01-02 16:47:53 +0100

    Added Task001.

---
+++ Task001/Task001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Task001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result = (from i in Enumerable.Range(1, 999)
+                     where (i % 3 == 0 || i % 5 == 0) 
+                     select i).Sum();
+
+            Console.WriteLine("Solution: {0}", result);
+
+        }
+    }
+}
