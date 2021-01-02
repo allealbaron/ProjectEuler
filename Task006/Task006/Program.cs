213ea0fe6c42915683e483d7688474ed9a1ecb3b
Angel <angel.luis.leal.baron@gmail.com>
2021-01-02 17:03:04 +0100

    Added Task006.

---
+++ Task006/Task006/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Task006
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int maxItem = 100;
+            
+            Int64 secondTerm = (from i in Enumerable.Range(1, maxItem)
+                     select i * i).Sum();
+
+            Int64 firstTerm = (from i in Enumerable.Range(1, maxItem)
+                                select i).Sum();
+
+            Console.WriteLine("Solution: {0}", (firstTerm * firstTerm) -secondTerm);
+
+        }
+    }
+}
