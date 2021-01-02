baae891cd367cf70a6e526b1b438585083412fe8
Angel <angel.luis.leal.baron@gmail.com>
2021-01-02 17:13:56 +0100

    Added Task014

---
+++ Task014/Task014/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Task014
+{
+    class Program
+    {
+        /// <summary>
+        /// Recursive function, gets the number of jumps 
+        /// </summary>
+        /// <param name="number">Number</param>
+        /// <param name="jumpsTaken">Jumps taken</param>
+        /// <returns>Number of jumps</returns>
+        static int GetNumberJumps(Int64 number, int jumpsTaken)
+        {
+            int result = 0;
+
+            if (number == 1)
+            {
+                result = jumpsTaken;
+            }
+            else
+            {
+                if (number % 2 == 0)
+                {
+                    jumpsTaken++;
+                    result = GetNumberJumps(number / 2, jumpsTaken);
+                }
+                else
+                {
+                    jumpsTaken++;
+                    result = GetNumberJumps((3*number)+1, jumpsTaken);
+                }
+            }
+            return result;
+        }
+        static void Main()
+        {
+            int solution = (from n in Enumerable.Range(1, 1000000)
+                     select new 
+                        { n, 
+                         jumps = GetNumberJumps(n, 1) }
+                     ).OrderByDescending(p => p.jumps).First().n;
+            
+            Console.WriteLine("Solution: {0}", solution);
+        }
+    }
+}
