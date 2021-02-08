406eb47aad3957bc1894b0e1f8bd90b15c8f745e
Angel <angel.luis.leal.baron@gmail.com>
2021-02-08 07:01:46 +0100

    Added Task 067

---
+++ Task067/Task067/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace Task067
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<List<int>> Triangles = new List<List<int>>();
+
+            const Int32 BufferSize = 128;
+            FileStream fs = File.OpenRead("triangle.txt");
+            StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8, true, BufferSize);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] items = line.Split(' ');
+
+                Triangles.Add((from i in items select int.Parse(i)).ToList<int>());
+
+            }
+
+            sr.Close();
+            fs.Close();
+
+            for (int i = Triangles.Count - 2; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Triangles[i][j] += Math.Max(Triangles[i + 1][j], Triangles[i + 1][j + 1]);
+                }
+            }
+
+            Console.WriteLine("Solution: {0}", Triangles[0][0]);
+
+        }
+    }
+}
