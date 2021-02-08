cc602e8668ebae0996537a2fa058cea05971a507
Angel <angel.luis.leal.baron@gmail.com>
2021-02-08 07:00:14 +0100

    Added Task 012

---
+++ Task012/Task012/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utilities.Math;
+
+namespace Task012
+{
+    class Program
+    {
+        /// <summary>
+        /// Main Thread
+        /// </summary>
+        static void Main()
+        {
+            Int64 i = 0;
+            List<int> divisors = new List<int>();
+            string solution = String.Empty;
+
+            while (divisors.Count<500)
+            {
+                i++;
+
+                TriangularNumber.AddNextTerm();
+
+                solution = TriangularNumber.GetListTriangularNumber().Last();
+
+                divisors = PrimeNumber.GetListDivisors(Int64.Parse(solution));
+
+            }
+
+
+            Console.WriteLine("Solution: {0}", solution);
+        }
+    }
+}
